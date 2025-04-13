7bc086b57ff9036f2a98ac47538a6f0d658582ba
Hami-Le <hami170106@gmail.com>
2025-04-13 18:21:55 +0700

    .

--- JavaThread/Ex3.java
+++ JavaThread/Ex3.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Ex3;
 
 class Thread1 extends Thread {
